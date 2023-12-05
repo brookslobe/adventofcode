724143a596212ff2613de16db9e382600c5f3280
Brooks Lobe <14917976+brookslobe@users.noreply.github.com>
2023-12-04 21:15:11 -0800

    2023 with csharp day 1

---
+++ 2023/01/Program.cs
@@ -0,0 +1,91 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int sumTotal = 0;
+
+Dictionary<string, int> conversions = new Dictionary<string, int>()
+{
+    {"one", 1},
+    {"two", 2},
+    {"three", 3},
+    {"four", 4},
+    {"five", 5},
+    {"six", 6},
+    {"seven", 7},
+    {"eight", 8},
+    {"nine", 9},
+};
+
+try {
+    using (StreamReader sr = new StreamReader("input.txt")) {
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            List<string> possibleWords = new List<string>();
+
+            Console.WriteLine("******");
+            Console.WriteLine(line);
+            
+            int number = 0;
+            int firstNum = 0;
+            int lastNum = 0;
+            bool numSeen = false;
+            string currentWord = "";
+            foreach (char c in line) {
+                bool isNum = int.TryParse(c.ToString(), out number);
+                if (isNum) {
+                    possibleWords.Clear();
+                    if (numSeen)
+                        lastNum = number;
+                    else {
+                        firstNum = number;
+                        numSeen = true;
+                    }
+                }
+                else {
+
+
+                    currentWord += c;
+                    Console.WriteLine(currentWord);
+
+                    // possibleWords = possibleWords.ForEach(pw => pw = pw += c);
+                    possibleWords = possibleWords.Select(pw => string.Concat(pw, c)).ToList();
+                    possibleWords.Add(c.ToString());
+
+
+                    var matches = conversions.Keys.Intersect(possibleWords);
+                    if (matches.Count() > 0) {
+                        // number = conversions[currentWord];
+                        string numberWord = matches.First();
+                        Console.WriteLine($"INTERSECTION: {numberWord}");
+                        number = conversions[numberWord];
+                        if (numSeen)
+                            lastNum = number;
+                        else {
+                            firstNum = number;
+                            numSeen = true;
+                        }
+                        currentWord = "";
+                        possibleWords.Clear();
+                    } 
+                    // else {
+                    //     possibleWords.ForEach(pw => pw = pw += c);
+                    //     possibleWords.Add(c.ToString());
+                    // }
+                }
+            }
+            if (lastNum == 0)
+                lastNum = firstNum;
+
+            Console.WriteLine($"{firstNum} and {lastNum}");
+            string concatNums = $"{firstNum}{lastNum}";
+            Console.WriteLine(concatNums);
+            sumTotal += int.Parse(concatNums);
+        }
+    }
+}
+catch (Exception ex) {
+    Console.WriteLine(ex.Message);
+}
+
+Console.WriteLine($"sum total: {sumTotal}");
---
+++ 2023/experience.md
@@ -0,0 +1,14 @@
+# Takeaways from AoC 2023
+
+## Including 
+- AoC process itself
+- Python familiarization
+- Code modularity/flexibity...hindsight improvements
+
+## Day 01
+- C# definitely harder than python to do scrappy things with
+- "simple main" and top level statements easier for scrappy things
+- `dotnet` CLI works well for simple commands like `dotnet run`
+- Need a better way to separate files for two halves of the day for clarity afterwards
+- C# in vscode without intellisense extension is rough lol
+- Intersect() seems like a pretty ineffiecient approach...probably something better
